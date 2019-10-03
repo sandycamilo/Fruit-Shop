b4e64b6399da31ee9877779caf80183038cd0e91
sandycamilo <scam499@lasierra.edu>
2019-10-03 16:41:24 -0700

    Added MongoDB and Fruit model

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask 
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.FruitShop
+fruits = db.fruits
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-    """Homepage"""
-    return render_template('home.html', msg= "Hi, welcome to the Fruit Shop!")
+def fruits_index ():
+    return render_template('fruits_index.html', fruits = fruits.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
