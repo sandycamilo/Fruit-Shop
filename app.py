2875415415af1b0d8a26fd442e3d457fd960832e
sandycamilo <scam499@lasierra.edu>
2019-10-03 18:57:02 -0700

    Go to pick fruits

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
-db = client.FruitShop
+db = client.TheFruitShop
 fruits = db.fruits
 
 app = Flask(__name__)
@@ -11,6 +11,16 @@ app = Flask(__name__)
 @app.route('/')
 def fruits_index ():
     return render_template('fruits_index.html', fruits = fruits.find())
+
+@app.route('/fruits/selectfruit')
+def fruits_selectfruit():
+    return render_template('fruits_selectfruit.html')
+
+@app.route('/fruits', methods=['GET'])
+def fruits_submit():
+    print(request.form.to_dict())
+    return redirect(url_for('fruits_index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
