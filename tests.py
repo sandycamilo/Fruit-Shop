25f2c63e995d9103615d099387e4ed5af8c64371
sandycamilo <scam499@lasierra.edu>
2019-10-10 23:45:45 -0700

    tests

--- tests.py
+++ tests.py
@@ -17,5 +17,24 @@ class FruitShopTests(TestCase):
         self.assertEqual(result.status, '200 OK')
         self.assertIn(b'Fruit', result.data)
 
+    def test_selectfruit(self):
+        """Test the select fruit page."""
+        result = self.client.get('/fruits/selectfruit')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'selectfruit', result.data)
+
+    def test_checkout(self):
+        """Test the checkout page."""
+        result = self.client.get('/fruits/checkout')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'checkout', result.data)
+
+    def test_tobasket(self):
+        """Test the basket page."""
+        result = self.client.get('/fruits/tobasket')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'tobasket', result.data)
+    
+
 if __name__ == '__main__':
     unittest_main()
