7be9ab1b2855d46b314bb6fba850285c8e3511ff
sandycamilo <scam499@lasierra.edu>
2019-10-10 21:44:45 -0700

    can see comments

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/fruitshop')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 fruits = db.fruits
+comments = db.comments
 
 app = Flask(__name__)
 
@@ -59,6 +60,23 @@ def fruits_submit():
 def fruits_thankyou():
     return render_template('fruits_thankyou.html')
 
+@app.route('/fruits/comments', methods=['POST'])
+def comments_new():
+    comment = {
+        'title': request.form.get('title'),
+        'content': request.form.get('content'),
+        'fruit_id': ObjectId(request.form.get('fruit_id'))
+    }
+    print(comment)
+    comment_id = comments.insert_one(comment).inserted_id
+    return redirect(url_for('fruits_show', fruit_id=request.form.get('fruit_id')))
+    return 'Leave a review'
+
+@app.route('/fruits/<fruit_id>')
+def fruits_show(fruit_id):
+    fruit = fruits.find_one({'_id': ObjectId(fruit_id)})
+    fruit_comments = comments.find({'fruit_id': ObjectId(fruit_id)})
+    return render_template('fruits_show.html', fruit=fruit, comments=fruit_comments)
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
